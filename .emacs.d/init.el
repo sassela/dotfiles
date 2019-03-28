a8996e84bda907f0d5f32b18b6233164bf0ee08d
sassela <sassela@users.noreply.github.com>
2019-03-28 08:19:59 +0000

    Remove intero references

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -56,7 +56,6 @@
     haskell-stylish-on-save t
     hindent-reformat-buffer-on-save t
     ;; Haskell interactive mode
-    haskell-process-args-stack-ghci '("--ghc-options=-ferror-spans --no-build --no-load")
     haskell-process-auto-import-loaded-modules t
     haskell-process-suggest-hoogle-imports t
     haskell-process-suggest-remove-import-lines t
@@ -479,12 +478,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (psc-ide dumb-jump smex highlight-sexp expand-region intero docker-compose-mode docker psci clj-refactor cider avy haskell-mode go-mode clojure-mode exec-path-from-shell flatland-theme align-cljlet buffer-move jvm-mode inf-ruby git-gutter kaesar yagist yasnippet yaml-mode use-package undo-tree smartparens shm repl-toggle projectile paredit mustache-mode multiple-cursors markdown-mode magit ido-completing-read+ idle-highlight-mode hl-sexp hindent golden-ratio go-eldoc ghc flyspell-correct flycheck-haskell elm-mode dockerfile-mode browse-kill-ring ansible ag ace-flyspell)))
+    (psc-ide dumb-jump smex highlight-sexp expand-region docker-compose-mode docker psci clj-refactor cider avy haskell-mode go-mode clojure-mode exec-path-from-shell flatland-theme align-cljlet buffer-move jvm-mode inf-ruby git-gutter kaesar yagist yasnippet yaml-mode use-package undo-tree smartparens shm repl-toggle projectile paredit mustache-mode multiple-cursors markdown-mode magit ido-completing-read+ idle-highlight-mode hl-sexp hindent golden-ratio go-eldoc ghc flyspell-correct flycheck-haskell elm-mode dockerfile-mode browse-kill-ring ansible ag ace-flyspell)))
  '(safe-local-variable-values
    (quote
-    ((intero-stack-yaml . "/Users/sassela/.stack/config.yaml")
-     (intero-targets "aeas:lib" "aeas:exe:aeas-exe" "aeas:test:aeas-test")
-     (eval define-clojure-indent
+    ((eval define-clojure-indent
            (snippet
             (quote defun))
            (template
